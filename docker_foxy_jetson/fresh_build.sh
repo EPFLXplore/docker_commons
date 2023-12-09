e4f4aa4cd9ddaf864938392f9c6a71da71c06ea1
hmiranda-queiros <hugomirandaqueiros@gmail.com>
2023-12-09 20:30:10 +0100

    feat: Add Jetson humble docker image

--- docker_foxy_jetson/fresh_build.sh
+++ docker_foxy_jetson/fresh_build.sh
@@ -1,1 +1,1 @@
-docker build --pull --no-cache --progress=plain -t ghcr.io/epflxplore/docker_commons:foxy-jetson --target xplore_common .
+docker build --pull --no-cache --progress=plain -t ghcr.io/epflxplore/docker_commons:foxy-jetson --target gazebo_nvidia .
