03ec6afa070d068d505b2f653b95c99ea34ce363
Ugo Balducci <51507845+TheGuyWithoutH@users.noreply.github.com>
2024-08-03 14:53:53 +0000

    fix: ros_entrypoint missing script

--- docker_humble_jetson_torch/ros2_install.sh
+++ docker_humble_jetson_torch/ros2_install.sh
@@ -51,10 +51,10 @@ colcon build ${COLCON_FLAGS} --base-paths src --event-handlers console_direct+
 
 #rm ${ROS_WORKSPACE}/*.rosinstall
 
-if grep $ROS_WORKSPACE ./ros_entrypoint.sh; then
+if grep $ROS_WORKSPACE /home/ros_entrypoint.sh; then
     echo "workspace $ROS_WORKSPACE was already set to be sourced on startup:"
 else
-    tac ./ros_entrypoint.sh | sed -e "3iros_source_env $ROS_WORKSPACE/install/setup.bash" | tac | tee ./ros_entrypoint.sh
+    tac /home/ros_entrypoint.sh | sed -e "3iros_source_env $ROS_WORKSPACE/install/setup.bash" | tac | tee /home/ros_entrypoint.sh
     echo "added $ROS_WORKSPACE to be sourced on startup:"
 fi
 
